2f030cdd48db3e7679b45c69ef1ac7221431933f
AngeDevhb <angehebard@gmail.com>
2026-07-17 16:27:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "",     // this is what people actually see
+        state: "0",     // this is what people actually see
         type: 0,               // Custom
       },
     ],
